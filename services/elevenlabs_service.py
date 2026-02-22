18ea9754752e4782ec93780e04f3d8ee9de7f078
julianias14 <julianias14@gmail.com>
2026-02-21 20:10:58 -0500

    fjksdjfdsklnfeislndkls

--- services/elevenlabs_service.py
+++ services/elevenlabs_service.py
@@ -2,21 +2,37 @@
 import os
 from dotenv import load_dotenv
 from io import BytesIO
-import requests
+from flask import Flask, request, jsonify
 from elevenlabs.client import ElevenLabs
 
 load_dotenv()
 
-elevenlabs = ElevenLabs(
-  api_key=os.getenv("ELEVENLABS_API_KEY"),
-)
+app = Flask(__name__)
 
-audio_url = (
+#Elevenlabs set up
+eleven = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
+
+#Supabase setup
+'''audio_url = (
     "https://storage.googleapis.com/eleven-public-cdn/audio/marketing/nicole.mp3"
 )
-response = requests.get(audio_url)
-audio_data = BytesIO(response.content)
+response = request.get(audio_url)
+audio_data = BytesIO(response.content)'''
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv()
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+@app.route("/transcribe", methods=["POST"])
+
+def transcribe():
+    audio_file = request.files["audio"]
+    response = eleven.speech_to_text.convert(file = audio_file)
+
+    transcript = response.text
+
+    #Inserting transcript into Supabase
+
+    supabase.table("text_entries".insert)
 transcription = elevenlabs.speech_to_text.convert(
     file=audio_data,
     model_id="scribe_v2", # Model to use
